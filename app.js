0359311c92dce6f575b4c8253b6a5ef2dfadc979
Kevin Eanes <kevineanes@mail.fresnostate.edu>
2017-04-02 10:31:55 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose');
 
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req,res){
+
+        var query = {};
+
+        if(req.query.genre)
+        {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function(err,books){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(books);
+        })
+    });
+
+bookRouter.route('/Books/:bookID')
+    .get(function(req,res){
+
+   
+   
+        Book.findById(rq.params.bookID, function(err,book){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(book);
+        })
+    });
+    
+app.use('/api', bookRouter);
+
 
 app.get('/', function(req, res){
     res.send('Welcome to my API!');
